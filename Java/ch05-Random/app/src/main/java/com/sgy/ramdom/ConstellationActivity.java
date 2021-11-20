db918a688b5960b9b943fa49cd6433cb2d0073c4
송가영 <sgyoung96@naver.com>
2021-11-20 23:01:40 +0900

    activity 생성

---
+++ Java/ch05-Random/app/src/main/java/com/sgy/ramdom/ConstellationActivity.java
@@ -0,0 +1,14 @@
+package com.sgy.ramdom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConstellationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constellation);
+    }
+}
