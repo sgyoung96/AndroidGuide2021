835121458b5440e0a86c4d44cf28f872b324f548
송가영 <sgyoung96@naver.com>
2021-11-20 23:08:51 +0900

    암시적 Intent 성공 (Uri)

--- Java/ch05-Intent/app/src/main/java/com/sgy/intenturi/MainActivity.java
+++ Java/ch05-Intent/app/src/main/java/com/sgy/intenturi/MainActivity.java
@@ -2,13 +2,28 @@ package com.sgy.intenturi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+
+import com.sgy.intenturi.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.tvMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.naver.com")));
+            }
+        });
     }
 }
